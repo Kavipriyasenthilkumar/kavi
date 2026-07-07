b455e2b2537e9c91f272ff86becef68f3ae9db92
Kavipriyasenthilkumar <kavipriyasendhil@gmail.com>
2026-07-07 11:20:18 +0530

    Add files via upload

---
+++ kavi.pyhton.py
@@ -0,0 +1,45 @@
+x=20
+x*=2
+x/=1
+m=10
+n=20
+print(m==n)
+print(m>n)
+print(m<=n)
+print(m>=n)
+print(a>=10 and b<=2)
+print(a>=10 or  b<=2)
+print(not a>=10)
+a=[1,2,3,4,5,8]
+print(6 in a)
+print(9 in a)
+print(9 not in a)
+print(6 not in a)
+x=10
+y=10
+z=11
+print(x is y)
+print(x is z)
+print(x is not z)
+n=10
+print(~n)
+n=-10
+print(~n)
+print(5&3)
+print(5|3)
+print(5^3)
+print(5<<2)
+print(5>>2)
+print("START")
+x=10
+y=20
+z=x+y
+print("SUM:",z)
+print("END")
+
+
+
+
+
+
+
